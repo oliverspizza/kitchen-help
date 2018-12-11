a9cf8fc30967088e73d748cf31f1539758891f61
Joli <john@oliverspizza.com>
2018-12-10 22:39:14 -0500

    added user

--- preplist/prep/accounts/models.py
+++ preplist/prep/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
 
+)
 # Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, unique=True)
+    phone_number = models.IntegerField()
+
+
+class DayOff(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    day_not_avaliable = models.DateField()
